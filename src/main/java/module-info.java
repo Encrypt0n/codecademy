363549aa9950f57ff91ecad7920f61c8c7cba351
Bas van Turnhout <ba.vanturnhout@student.avans.nl>
2021-12-16 13:29:36 +0100

    Queries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.omega13.codecademy {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
 
     opens com.omega13.codecademy to javafx.fxml;
