2c723e469163cc3f81ce9d0e55ba6e3dd5c9cd0c
42837230+RMvanderGaag@users.noreply.github.com <42837230+RMvanderGaag@users.noreply.github.com>
2022-01-15 14:14:32 +0100

    Merged bas into rogier

--- src/main/java/com/omega13/codecademy/controllers/progress/ProgressCourseController.java
+++ src/main/java/com/omega13/codecademy/controllers/progress/ProgressCourseController.java
@@ -1,39 +1,143 @@
 package com.omega13.codecademy.controllers.progress;
 
 import com.omega13.codecademy.controllers.SceneController;
+import com.omega13.codecademy.database.CourseData;
+import com.omega13.codecademy.database.CourseMemberData;
+import com.omega13.codecademy.database.ModuleData;
+import com.omega13.codecademy.database.ProgressData;
+import com.omega13.codecademy.domain.Course;
+import com.omega13.codecademy.domain.CourseMember;
+import com.omega13.codecademy.domain.Progress;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ProgressCourseController {
+public class ProgressCourseController implements Initializable {
+    @FXML
+    Slider ProgressSlider;
+    @FXML
+    TableView<CourseMember> MemberTable;
+    @FXML
+    TableColumn<CourseMember, String> MemberColumn;
+
+    @FXML
+    TableView<Course> CourseTable;
+    @FXML
+    TableColumn<Course, String> CourseColumn;
+
     @FXML
     Button btn_return;
 
     @FXML
-    Button btn_modules;
+    TableView<com.omega13.codecademy.domain.Module> ModuleTable;
+    @FXML
+    TableColumn<com.omega13.codecademy.domain.Module, String> ModuleColumn;
+
+    CourseMemberData courseMemberData = new CourseMemberData();
+    CourseData courseData = new CourseData();
+    ModuleData moduleData = new ModuleData();
+    ProgressData progressData = new ProgressData();
+    SceneController sceneController = new SceneController();
+
+    int memberId;
+    int courseId;
+    int moduleId;
+    int contentId;
+    Progress progress;
+    int id;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        fillMemberList();
+
+    }
+
+    private void fillMemberList(){
+        MemberColumn.setCellValueFactory(data -> new SimpleStringProperty(this.courseMemberData.getCourseMember(data.getValue().getId()).getName()));
+        MemberTable.getItems().setAll(courseMemberData.getCourseMembers());
+        MemberTable.getSelectionModel().selectedIndexProperty().addListener((num) -> getCourses());
+    }
+
+    private void getCourses() {
+        if(MemberTable.isPressed()){
+            memberId = MemberTable.getSelectionModel().getSelectedItem().getId();
+            this.CourseColumn.setCellValueFactory(data -> new SimpleStringProperty(this.courseData.getCourse(data.getValue().getId()).getTitle()));
+            CourseTable.getItems().setAll(courseData.getCoursesPerMember(memberId));
+            CourseTable.getSelectionModel().selectedIndexProperty().addListener((num) -> getModules());
+            //this.CourseColumn.setCellValueFactory(data -> new SimpleStringProperty(this.courseData.getCourse(data.getValue().getId()).getTitle()));
+            //CourseTable.getItems().setAll(courseData.getCoursesPerMember((memberId)));
+        }
+
+    }
+
+    private void getModules() {
+        if(CourseTable.isPressed()){
+            courseId = CourseTable.getSelectionModel().getSelectedItem().getId();
+            this.ModuleColumn.setCellValueFactory(data -> new SimpleStringProperty(this.moduleData.getModule(data.getValue().getId()).getTitle()));
+            //System.out.println(moduleData.getModulesPerCourse(courseId));
+            ModuleTable.getItems().setAll(moduleData.getModulesPerCourse(courseId));
+            ModuleTable.getSelectionModel().selectedIndexProperty().addListener((num) -> getProgress());
+
+
+            /*int courseId = CourseTable.getSelectionModel().getSelectedItem().getId();
+            this.ModuleColumn.setCellValueFactory(data -> new SimpleStringProperty(this.moduleData.getModule(data.getValue().).getTitle()));
+            //this.ModuleColumn.setCellValueFactory(data -> new SimpleStringProperty(this.moduleData.getModule(data.getValue().getId()).getTitle()));
+            ModuleTable.getItems().setAll((Module) progressData.getModules(courseId));*/
+        }
+
+    }
+
+    private void getProgress() {
+        if(ModuleTable.isPressed()){
+            moduleId = ModuleTable.getSelectionModel().getSelectedItem().getId();
+            //this.CourseColumn.setCellValueFactory(data -> new SimpleStringProperty(this.progressData.getProgress(data.getValue().getId()).toString());
+            
+            progress = this.progressData.getProgress(moduleId, memberId);
+
+
+            if(progress != null) {
+                //System.out.println(progress.getPercentage());
+                id = progress.getId();
+                contentId = progress.getContentID();
+                ProgressSlider.adjustValue(progress.getPercentage());
+
+            }
+            //CourseTable.getItems().setAll(courseData.getCoursesPerMember(moduleId));
+            //CourseTable.getSelectionModel().selectedIndexProperty().addListener((num) -> getModules());
+            //this.CourseColumn.setCellValueFactory(data -> new SimpleStringProperty(this.courseData.getCourse(data.getValue().getId()).getTitle()));
+            //CourseTable.getItems().setAll(courseData.getCoursesPerMember((memberId)));
+        }
+
+    }
 
     @FXML
-    Button btn_webcasts;
+    public void saveProgress(ActionEvent e){
+        System.out.println("hoi");
+        System.out.println(memberId);
+        progressData.addProgress((int)ProgressSlider.getValue(), memberId, contentId, moduleId);
+        progressData.addCertitifcate(courseId, memberId, moduleId);
+        //CourseMembers.refresh();
+    }
 
-    SceneController sceneController;
+    @FXML
+    public void onSliderChanged(){
+        int sliderValue = (int) ProgressSlider.getValue();
 
-    public ProgressCourseController() {
-        this.sceneController = new SceneController();
+        System.out.println(sliderValue);
     }
 
     @FXML
     private void onReturnClick() throws IOException {
-        sceneController.sceneSwitcher("home-view.fxml", btn_return);
-    }
-
-    @FXML
-    private void onModulesClick() throws IOException {
-        sceneController.sceneSwitcher("progress/progresscourse-view.fxml", btn_modules);
-    }
-
-    @FXML
-    private void onWebcastsClick() throws IOException {
-        sceneController.sceneSwitcher("progress/progresswebcast-view.fxml", btn_webcasts);
+        sceneController.sceneSwitcher("progress/progress-view.fxml", btn_return);
     }
 }
